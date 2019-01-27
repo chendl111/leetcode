2ca438f4460e6621b78536a35f60e930adc09dee
chendl <571134177@qq.com>
2019-01-27 12:27:51 +0800

    default

--- algorithms/index.sh
+++ algorithms/index.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
+echo "|#|Title|Solution|" >> ../README.md
+echo "|-|-----|--------|" >> ../README.md
 ls | grep '^[0-9]'|sort -n|while read line
 do
     title=`echo ${line}|cut -d . -f 2`
     declare -l url=${title}
-    echo "|${line%%.*}|[$title](https://leetcode-cn.com/problems/algorithms/url//' '/'-'}/description/)|[C++](https://github.com/chendl111/leetCode/blob/master/algorithms/${line//' '/'%20'})|" >>../README.md 
+    echo "|${line%%.*}|[$title](https://leetcode-cn.com/problems/algorithms/${url//' '/'-'}/description/)|[C++](https://github.com/chendl111/leetCode/blob/master/algorithms/${line//' '/'%20'})|" >>../README.md 
 done
